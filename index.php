a155cdd8f48f3044177fce4a2a141bdcd22dda0d
Cristobal Dominguez <cristobald@me.com>
2016-12-06 17:00:02 -0300

    Adds index html structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0" />
+	<title></title>
+	<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/style.css">
+	<?php wp_head() ?>
+</head>
+<body>
+	
+<?php wp_footer() ?>
+</body>
+</html>
