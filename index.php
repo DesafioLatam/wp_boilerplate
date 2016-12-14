e0a83e58119319be3ae94cbef8666e2919204b16
Cristobal Dominguez <cristobald@me.com>
2016-12-13 23:36:32 -0300

    Upgrades head structure with WP scripts

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="<?php bloginfo('language') ?>">
 <head>
-	<meta charset="utf-8" />
+	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0" />
 	<title></title>
 	<?php wp_head() ?>
