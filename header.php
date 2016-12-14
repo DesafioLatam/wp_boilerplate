2a486fb8a8ea7601a2e675a7bde97a04d21dc475
Cristobal Dominguez <cristobald@me.com>
2016-12-14 11:04:08 -0300

    Adds Site Name to Title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0" />
-	<title></title>
+	<title><?php bloginfo('name') ?></title>
 	<?php wp_head() ?>
 	<?php get_template_part('_includes/iOS', 'icons') ?>
 </head>
