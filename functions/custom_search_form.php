332438de6eaecb83634b421f5aadbd411c51a4d3
Cristobal Dominguez <cristobald@me.com>
2016-12-14 11:19:30 -0300

    Adds a customizable search form

---
+++ functions/custom_search_form.php
@@ -0,0 +1,20 @@
+<?php
+	
+	function custom_search_form() {
+		$form = '<div class="search">';
+		$form .= '<form role="search" method="get" class="search-form" action="';
+		$form .= get_bloginfo('url');
+		$form .= '">';
+		$form .= '<label>';
+		$form .= '<input type="search" class="search-field" placeholder="Buscador" value="" name="s" title="Buscar:" aria-label="Área de busqueda" />';
+		$form .= '</label>';
+		$form .= '<input type="submit" class="search-submit" value="&#xf002;" aria-label="Buscar" />';
+		$form .= '</form>';
+		$form .= '</div>';
+
+		echo $form;
+	}
+
+	//add_filter( 'get_search_form', 'custom_search_form' );
+
+?>
