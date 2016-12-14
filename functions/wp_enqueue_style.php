a3723cbcbd915aa458ddc806fe746541b219dace
Cristobal Dominguez <cristobald@me.com>
2016-12-14 10:57:33 -0300

    Includes a Reset CSS stylesheet

--- functions/wp_enqueue_style.php
+++ functions/wp_enqueue_style.php
@@ -1,7 +1,10 @@
 <?php 
 	
 	function enqueue_style() {
+		wp_register_style('reset', get_template_directory_uri() . '/assets/css/reset.css', null, null, null);
 		wp_register_style('mainStyle', get_template_directory_uri() . '/assets/css/style.css', null, null, null);
+		
+		wp_enqueue_style('reset');
 		wp_enqueue_style('mainStyle');
 	}
 
