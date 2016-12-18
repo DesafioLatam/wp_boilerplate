763ba3642f3120c86e9575121c3382913cad304f
Cristobal Dominguez <cristobald@gmail.com>
2016-12-18 12:22:56 -0300

    Updates nav registration

--- functions/add_theme_support.php
+++ functions/add_theme_support.php
@@ -4,6 +4,11 @@
 
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
+
+		register_nav_menus( array(
+			'header-menu'  => __( 'Header Menu' ),
+			'footer-menu'  => __( 'Footer Menu' ),
+		) );
 	}
 
 	add_action( 'after_setup_theme', 'dl_theme_setup' );
