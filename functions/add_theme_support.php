3c05caf1487c56e4ff0c661fdda953a46dcde85f
Cristobal Dominguez <cristobald@gmail.com>
2016-12-18 12:16:45 -0300

    Improves theme support

--- functions/add_theme_support.php
+++ functions/add_theme_support.php
@@ -1,5 +1,11 @@
 <?php 
 
-	add_theme_support( 'post-thumbnails' );
+	function dl_theme_setup() {
+
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+	}
+
+	add_action( 'after_setup_theme', 'dl_theme_setup' );
 	
 ?>
