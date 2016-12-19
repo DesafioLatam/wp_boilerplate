ab7cee91c73e1bf0c2704eaeb72ac34028732e8a
Cristobal Dominguez <cristobald@gmail.com>
2016-12-19 10:00:40 -0300

    Enable support for widget sidebars selective refresh in the Customizer

--- functions/add_theme_support.php
+++ functions/add_theme_support.php
@@ -4,6 +4,7 @@
 
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'customize-selective-refresh-widgets' );
 
 		register_nav_menus( array(
 			'header-menu'  => __( 'Header Menu' ),
