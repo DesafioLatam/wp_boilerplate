69e2122f753b3cfa8ffffe5e593f813cb0f561b5
Cristobal Dominguez <cristobald@gmail.com>
2016-12-19 10:02:15 -0300

    Enables support for WooCommerce

--- functions/add_theme_support.php
+++ functions/add_theme_support.php
@@ -5,6 +5,7 @@
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support( 'woocommerce' );
 
 		register_nav_menus( array(
 			'header-menu'  => __( 'Header Menu' ),
