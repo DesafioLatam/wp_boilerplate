3e0a4e6015b31471edad816e56a77ea5b4965c63
Cristobal Dominguez <cristobald@gmail.com>
2016-12-19 23:02:59 -0300

    Removes Unnecessary Code from wp_head

---
+++ functions/remove_action.php
@@ -0,0 +1,7 @@
+<?php 
+
+	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+
+?>
--- functions/wp_remove_version.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	function wp_remove_version() {
-		return '';
-	}
-
-	add_filter('the_generator', 'wp_remove_version');
-
-?>
