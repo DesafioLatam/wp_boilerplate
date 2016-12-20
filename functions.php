79d8f3b4dbbaf960ce67e937811df756994df0b3
Cristobal Dominguez <cristobald@gmail.com>
2016-12-20 08:22:07 -0300

    Checks if current WP version is at least 4.7

--- functions.php
+++ functions.php
@@ -3,7 +3,19 @@
 	$files = glob(get_template_directory() . '/functions/*.php');
 
 	foreach($files as $file) {
-		require_once $file;
+		if (basename($file, ".php") != 'back-compat') {
+			require_once $file;
+		}
 	}
-	
+
+
+	/**
+	 * This Theme only works in WordPress 4.7 or later.
+	*/
+
+	if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
+		require get_template_directory() . '/functions/back-compat.php';
+		return;
+	}
+
 ?>
