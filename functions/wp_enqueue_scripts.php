6d6f84acc8c9fba347b0d35cdda0ed1bfaa4761c
Cristobal Dominguez <cristobald@gmail.com>
2016-12-28 10:33:57 -0300

    Adds jQuery backward compatibility

--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -6,11 +6,17 @@
 		wp_deregister_script( 'jquery-migrate' );
 
 		/* Register Scripts */
-		wp_register_script('custom_jQuery', get_template_directory_uri() . '/assets/js/lib/jquery-3.1.1.min.js', null, null, true);
-		wp_register_script('mainJS', get_template_directory_uri() . '/assets/js/functions.js', 'custom_jQuery', null, true);
+		//wp_register_script('jQuery1', get_template_directory_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
+		wp_register_script('jQuery3', get_template_directory_uri() . '/assets/js/lib/jquery-3.1.1.min.js', null, null, true);
+		wp_register_script('jQuery_migrate', get_template_directory_uri() . '/assets/js/lib/jquery-migrate-3.0.0.min.js', 'jQuery3', null, true);
+		wp_register_script('flexslider', get_template_directory_uri() . '/assets/js/lib/jquery.flexslider-min.js', 'jQuery3', null, true);
+		wp_register_script('mainJS', get_template_directory_uri() . '/assets/js/functions.js', 'jQuery3', null, true);
 		
 		/* Enqueue Scripts */
-		wp_enqueue_script('custom_jQuery');
+		//wp_enqueue_script('jQuery1');
+		wp_enqueue_script('jQuery3');
+		wp_enqueue_script('jQuery_migrate');
+		wp_enqueue_script('flexslider');
 		wp_enqueue_script('mainJS');
 	}
 
