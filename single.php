f99389f089dafa012b29489c8fe13b6f88f87c89
Cristobal Dominguez <cristobald@gmail.com>
2016-12-29 15:58:00 -0300

    Adds a loop in single file

--- single.php
+++ single.php
@@ -1,8 +1,13 @@
 <?php get_header() ?>
-	<?php the_post() ?>
 
-	<h1 class="title"><?php the_title() ?></h1>
-	<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
-	<?php the_content(); ?>
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>    
+		
+			<h1 class="title"><?php the_title() ?></h1>
+			<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
+			<?php the_content(); ?>
+
+		<?php endwhile; ?>
+	<?php endif; ?>
 
 <?php get_footer() ?>
