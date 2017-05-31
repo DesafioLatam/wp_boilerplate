60003738568904794260a7c1641c1daac5a65272
Cristobal Dominguez <cristobald@gmail.com>
2017-05-31 18:42:44 -0400

    Upgrades some functions to Wordpress 4.7

--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -6,11 +6,11 @@
 		wp_deregister_script( 'jquery-migrate' );
 
 		/* Register Scripts */
-		//wp_register_script('jQuery1', get_template_directory_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
-		wp_register_script('jQuery3', get_template_directory_uri() . '/assets/js/lib/jquery-3.1.1.min.js', null, null, true);
-		wp_register_script('jQuery_migrate', get_template_directory_uri() . '/assets/js/lib/jquery-migrate-3.0.0.min.js', 'jQuery3', null, true);
-		wp_register_script('flexslider', get_template_directory_uri() . '/assets/js/lib/jquery.flexslider-min.js', 'jQuery3', null, true);
-		wp_register_script('mainJS', get_template_directory_uri() . '/assets/js/functions.js', 'jQuery3', null, true);
+		//wp_register_script('jQuery1', get_theme_file_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
+		wp_register_script('jQuery3', get_theme_file_uri() . '/assets/js/lib/jquery-3.1.1.min.js', null, null, true);
+		wp_register_script('jQuery_migrate', get_theme_file_uri() . '/assets/js/lib/jquery-migrate-3.0.0.min.js', 'jQuery3', null, true);
+		wp_register_script('flexslider', get_theme_file_uri() . '/assets/js/lib/jquery.flexslider-min.js', 'jQuery3', null, true);
+		wp_register_script('mainJS', get_theme_file_uri() . '/assets/js/functions.js', 'jQuery3', null, true);
 		
 		/* Enqueue Scripts */
 		//wp_enqueue_script('jQuery1');
--- functions/wp_enqueue_style.php
+++ functions/wp_enqueue_style.php
@@ -1,8 +1,8 @@
 <?php 
 	
 	function dl_enqueue_style() {
-		wp_register_style('reset', get_template_directory_uri() . '/assets/css/reset.css', null, null, null);
-		wp_register_style('mainStyle', get_template_directory_uri() . '/assets/css/style.css', null, null, null);
+		wp_register_style('reset', get_theme_file_uri() . '/assets/css/reset.css', null, null, null);
+		wp_register_style('mainStyle', get_theme_file_uri() . '/assets/css/style.css', null, null, null);
 		
 		wp_enqueue_style('reset');
 		wp_enqueue_style('mainStyle');
