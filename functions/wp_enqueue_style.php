b998d50a66a722f56fcfc5fed7bf02602fdcc031
Cristobal Dominguez <cristobald@gmail.com>
2017-07-26 16:31:15 -0400

    Improves WP Register Scripts

--- functions/wp_enqueue_style.php
+++ functions/wp_enqueue_style.php
@@ -4,8 +4,8 @@
 		$theme_data = wp_get_theme();
 
 		/* Register Scripts */
-		wp_register_style('reset', get_theme_file_uri() . '/assets/css/reset.css', null, $theme_data->get( 'Version' ), 'screen');
-		wp_register_style('mainStyle', get_theme_file_uri() . '/assets/css/style.css', array('reset'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('reset', get_parent_theme_file_uri('/assets/css/reset.css'), null, $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('mainStyle', get_parent_theme_file_uri('/assets/css/style.css'), array('reset'), $theme_data->get( 'Version' ), 'screen');
 		
 		/* Enqueue Scripts */
 		wp_enqueue_style('mainStyle');
