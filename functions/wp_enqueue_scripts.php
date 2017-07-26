6fcec6b49591665a549f01cca3bca7ea7e082391
Cristobal Dominguez <cristobald@gmail.com>
2017-07-26 16:36:15 -0400

    Updates Scripts Versions

--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -9,13 +9,12 @@
 
 		/* Register Scripts */
 		//wp_register_script('jQuery1', get_parent_theme_file_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
-		wp_register_script('jQuery3', get_parent_theme_file_uri('/assets/js/lib/jquery-3.1.1.min.js'), null, null, true);
-		wp_register_script('jQuery_migrate', get_parent_theme_file_uri('/assets/js/lib/jquery-migrate-3.0.0.min.js'), array('jQuery3'), null, true);
-		wp_register_script('flexslider', get_parent_theme_file_uri('/assets/js/lib/jquery.flexslider-min.js'), array('jQuery3','jQuery_migrate'), null, true);
-		wp_register_script('mainJS', get_parent_theme_file_uri('/assets/js/functions.js'), array('jQuery3','jQuery_migrate'), $theme_data->get( 'Version' ), true);
+		wp_register_script('jQuery3', get_parent_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.2.1', true);
+		wp_register_script('jQuery_migrate', get_parent_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jQuery3'), '3.0.0', true);
+		wp_register_script('flexslider', get_parent_theme_file_uri('/assets/js/lib/jquery.flexslider.min.js'), array('jQuery_migrate'), null, true);
+		wp_register_script('mainJS', get_parent_theme_file_uri('/assets/js/functions.js'), array('jQuery_migrate'), $theme_data->get( 'Version' ), true);
 		
 		/* Enqueue Scripts */
-		//wp_enqueue_script('jQuery1');
 		wp_enqueue_script('flexslider');
 		wp_enqueue_script('mainJS');
 	}
