c9195f1558e89dc791326a985cb7f410c2c5aee5
Cristobal Dominguez <cristobald@gmail.com>
2018-03-21 12:06:46 -0300

    Removes Flexslider's Load From Theme When Woocommerce is Active

--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -12,10 +12,10 @@
 			//wp_register_script('jQuery1', get_theme_file_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
 			wp_register_script('jQuery3', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.2.1', true);
 			wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jQuery3'), '3.0.0', true);
+			wp_register_script('flexslider', get_theme_file_uri('/assets/js/lib/jquery.flexslider.min.js'), array('jquery-migrate'), null, true);
 		}
 
 		/* Register Scripts */
-		wp_register_script('flexslider', get_theme_file_uri('/assets/js/lib/jquery.flexslider.min.js'), array('jquery-migrate'), null, true);
 		wp_register_script('mainJS', get_theme_file_uri('/assets/js/functions.js'), array('jquery-migrate'), $theme_data->get( 'Version' ), true);
 
 		/* Enqueue Scripts */
