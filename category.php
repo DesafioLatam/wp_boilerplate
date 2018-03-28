780c928baf3d22646cdb6204ee53baa442326732
Cristobal Dominguez <cristobald@gmail.com>
2018-03-28 01:28:45 -0300

    Adds Category File

---
+++ category.php
@@ -0,0 +1,24 @@
+<?php get_header() ?>
+
+	<?php if (have_posts()) { ?>
+
+	<ul>
+		<?php while ( have_posts() ) { the_post(); ?>
+		<li>
+			<a href="<?php the_permalink() ?>">
+				<?php the_post_thumbnail() ?>
+				<?php the_title() ?>
+				<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
+				<?php the_excerpt() ?>
+			</a>
+		</li>
+		<?php }; ?>
+	</ul>
+
+	<?php } else { ?>
+		<!-- Content -->
+		<p>No hay elementos</p>
+	<?php } wp_reset_query(); ?>
+
+	<?php get_sidebar() ?>
+<?php get_footer() ?>
