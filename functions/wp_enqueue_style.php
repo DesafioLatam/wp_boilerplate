1930c6277c55930eb0e105123d9794ce26f75c7d
Cristobal Dominguez <cristobald@gmail.com>
2018-03-28 00:48:33 -0300

    Replaces Flexslider with Flickity

--- functions/wp_enqueue_style.php
+++ functions/wp_enqueue_style.php
@@ -5,9 +5,11 @@
 
 		/* Register Scripts */
 		wp_register_style('reset', get_parent_theme_file_uri('/assets/css/reset.css'), null, $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('flickity', get_parent_theme_file_uri('/assets/css/flickity.css'), null, '2.1.0', 'screen');
 		wp_register_style('mainStyle', get_parent_theme_file_uri('/assets/css/style.css'), array('reset'), $theme_data->get( 'Version' ), 'screen');
 		
 		/* Enqueue Scripts */
+		wp_enqueue_style('flickity');
 		wp_enqueue_style('mainStyle');
 	}
 
