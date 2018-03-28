76b9132b5a92b2157c9f25ae53664e9a73a73590
Cristobal Dominguez <cristobald@gmail.com>
2018-03-28 01:11:43 -0300

    Removes Unused jQuery Versions

--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -9,7 +9,6 @@
 			wp_deregister_script( 'jquery-migrate' );
 
 			/* Register Scripts */
-			//wp_register_script('jQuery1', get_theme_file_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
 			wp_register_script('jQuery3', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.2.1', true);
 			wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jQuery3'), '3.0.0', true);
 		}
