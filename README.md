2dc1ac9dc2e2c733c5f79d0e2b97712475c7468a
Cristobal Dominguez <cristobald@gmail.com>
2018-03-28 01:08:46 -0300

    Replaces Flexslider with Flickity

--- README.md
+++ README.md
@@ -32,3 +32,10 @@ Para utilizar la paginación debemos llamar a la función `the_pagination()`. Y 
 ~~~
 
 Además es importante que dentro de los parámetros de búsqueda sea incorporado el key `paged` junto con el valor `$paged`, para que nuestra paginación pueda funcionar correctamente.
+
+###Uso de Flickity
+En la versión 1.3.0 se ha reemplazado Flexslider por [Flickity](https://github.com/metafizzy/flickity/), un versátil plugin de Javascript que contiene menos errores que el anterior, se encuentra más actualizado y además con más opciones de visualización. No depende de jQuery, por ende podremos trabajar en el Front-End con Vanilla JS (Javascript plano), sin tener que depender de otras librerías.
+
+Para obtener información de cómo implementar Flickity, por favor revisar la [documentación](https://flickity.metafizzy.co) correspondiente.
+
+
--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -12,14 +12,14 @@
 			//wp_register_script('jQuery1', get_theme_file_uri() . '/assets/js/lib/jquery-1.12.4.min.js', null, null, true);
 			wp_register_script('jQuery3', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.2.1', true);
 			wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jQuery3'), '3.0.0', true);
-			wp_register_script('flexslider', get_theme_file_uri('/assets/js/lib/jquery.flexslider.min.js'), array('jquery-migrate'), null, true);
 		}
 
 		/* Register Scripts */
+		wp_register_script('flickity', get_theme_file_uri('/assets/js/lib/flickity.pkgd.js'), array('jquery-migrate'), '2.1.0', true);
 		wp_register_script('mainJS', get_theme_file_uri('/assets/js/functions.js'), array('jquery-migrate'), $theme_data->get( 'Version' ), true);
 
 		/* Enqueue Scripts */
-		wp_enqueue_script('flexslider');
+		wp_enqueue_script('flickity');
 		wp_enqueue_script('mainJS');
 	}
 
