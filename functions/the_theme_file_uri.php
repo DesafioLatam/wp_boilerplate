a2cef32bbe97a2ac716b814c2de518c09caf45fe
Cristobal Dominguez <cristobald@gmail.com>
2018-07-19 18:15:46 -0400

    Avoids Conflicts with WP Creating the_theme_file_uri Function

--- functions/the_theme_file_uri.php
+++ functions/the_theme_file_uri.php
@@ -8,6 +8,10 @@
  * @return void
  * @since  1.1.6
  */
-function the_theme_file_uri( $file ) {
-	echo get_theme_file_uri( $file );
+if ( ! function_exists( 'the_theme_file_uri' ) ) {
+
+	function the_theme_file_uri( $file ) {
+		echo get_theme_file_uri( $file );
+	}
+
 }
