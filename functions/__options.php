5f892845a658f5443f9e074157ca5ae97c3241e8
Cristobal Dominguez <cristobald@gmail.com>
2018-10-05 13:14:20 -0300

    Improves Some Issues with jQuery Support

--- functions/__options.php
+++ functions/__options.php
@@ -9,7 +9,7 @@
  */
 $theme_options = array(
 	'wp_min_version'		=> '4.8',
-	'woocommerce_enabled'	=> class_exists( 'WooCommerce' ),
+	'woocommerce'			=> class_exists( 'WooCommerce' ),
 	'functions'				=> array(
 		'theme'				=> glob( get_template_directory() . '/functions/*.php' ),
 		'wc'				=> glob( get_template_directory() . '/functions/woocommerce/*.php' ),
--- functions/wp_enqueue_scripts.php
+++ functions/wp_enqueue_scripts.php
@@ -16,25 +16,27 @@ function dl_enqueue_scripts() {
 
 	global $theme_options;
 	$theme_data = wp_get_theme();
+	$deps = null;
 
 	/* Deregister Scripts */
 	wp_deregister_script( 'jquery' );
 	wp_deregister_script( 'jquery-migrate' );
 
 
-	/* Register Scripts */
-	wp_register_script( 'flickity', get_theme_file_uri('/assets/js/lib/flickity.pkgd.js'), array('jquery-migrate'), '2.1.0', true );
-	wp_register_script( 'flexslider', get_theme_file_uri('/assets/js/lib/jquery.flexslider.js'), array('jquery-migrate'), null, true );
-	wp_register_script( 'main_js', get_theme_file_uri('/assets/js/functions.js'), array('jquery-migrate'), $theme_data->get( 'Version' ), true );
-
-
-	if ( $theme_options['jQuery'] || $theme_options['woocommerce_enabled'] || $theme_options['slider']['flexslider'] || $theme_options['slider']['flickity'] ) {
+	/* Check jQuery Support */
+	if ( $theme_options['jQuery'] || $theme_options['woocommerce'] || $theme_options['slider']['flexslider'] || $theme_options['slider']['flickity'] ) {
 
 		wp_register_script('jquery', get_theme_file_uri('/assets/js/lib/jquery.min.js'), null, '3.3.1', true);
 		wp_register_script('jquery-migrate', get_theme_file_uri('/assets/js/lib/jquery-migrate.min.js'), array('jquery'), '3.0.0', true);
+		$deps = array('jquery-migrate');
 
 	}
 
+
+	/* Register Scripts */
+	wp_register_script( 'flickity', get_theme_file_uri('/assets/js/lib/flickity.pkgd.js'), array('jquery-migrate'), '2.1.0', true );
+	wp_register_script( 'flexslider', get_theme_file_uri('/assets/js/lib/jquery.flexslider.js'), array('jquery-migrate'), null, true );
+	wp_register_script( 'main_js', get_theme_file_uri('/assets/js/functions.js'), $deps, $theme_data->get( 'Version' ), true );
 
 	/* Enqueue Scripts */
 	if ( $theme_options['slider']['flexslider'] ) {
